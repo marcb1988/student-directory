63b51645d21e95d6fc870bc0489a3c0abc7e9274
Marc Blenkinsop <marcblenky@hotmail.com>
2019-10-02 14:45:18 +0100

    changed from array to hash and inserted input feature

--- directory.rb
+++ directory.rb
@@ -1,33 +1,41 @@
 # first we put the students into an array
-students = [
-"Dr. Hannibal Lecter",
-"Darth Vader",
-"Nurse Ratched",
-"Michael Corleone",
-"Alex DeLarge",
-"The Wicked Witch of the West",
-"Terminator",
-"Freddy Krueger",
-"The Joker",
-"Joffrey Baratheon",
-"Norman Bates"
-]
+
 # we created our methods
+def input_students
+    puts "Please enter the names of the students: "
+    puts "To finish, just hit return twice"
+    # we then create an empty array
+    students = []
+    # we now need to "gets" the names
+    name = gets.chomp
+    # while it is not empty, repeat the code
+    while !name.empty? do
+        # add the student to the array we created
+        students << {name: name, cohort: :november}
+        puts "Now we have #{students.count} students"
+        # then get the next name
+        name = gets.chomp
+    end
+    #return the array
+    students
+end
+
 def print_header
     puts "The students of Villains Academy"
     puts "---------"
 end
 
-def print(names)
-    names.each do |name|
-        puts name
+def print(students)
+    students.each do |student|
+        puts "#{student[:name]} (#{student[:cohort]} cohort)"
     end
 end
 
-def print_footer(names)
-    puts "Overall we have #{names.count} great students."
+def print_footer(students)
+    puts "Overall we have #{students.count} great students."
 end
 # then we call the methods
+students = input_students
 print_header
 print(students)
 print_footer(students)
