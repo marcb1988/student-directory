f845ffc0f8f127e8cdcb720d8d6b1209bc12180c
Marc Blenkinsop <marcblenky@hotmail.com>
2019-10-03 15:13:35 +0100

    defined method to sort students by cohort

--- directory.rb
+++ directory.rb
@@ -2,17 +2,32 @@ def input_students
     puts "Please enter the name of the student followed by the cohort and student age: "
     puts "To finish, just hit return twice"
     students = []
-    name = gets.chomp
-    cohort = gets.chomp.to_sym
+    name = gets.chomp.capitalize
+    cohort = gets.chomp.to_sym.upcase
     age = gets.chomp
 
     while !name.empty? do
+
+            if cohort.empty?
+                cohort = "unknown"
+            end
+
+            if age.empty?
+                age = "unknown"
+            end
+
         students << {name: name, cohort: cohort, age: age}
-        puts "Now we have #{students.count} students"
-        puts "Name? "
-        name = gets.chomp
+
+                if students.count < 2
+                    puts "We only have 1 student"
+                else 
+                    puts "Now we have #{students.count} students"
+                end
+
+        puts "More students?"
+        name = gets.chomp.capitalize
             break if name.empty?
-        cohort = gets.chomp
+        cohort = gets.chomp.to_sym.upcase
         age = gets.chomp
     end
     students
@@ -36,7 +51,14 @@ def print_footer(students)
     puts "Overall we have #{students.count} great students.".center(50)
 end
 
+def sort_by_cohort(students)
+    # puts "Which Cohort would you like to see? "
+    # cohort = gets.chomp.to_sym
+    students.sort_by { |student| student[:cohort] }.map { |student| puts "#{student[:name]} - #{student[:cohort]} - #{student[:age]}".center(50) }
+end
+
 students = input_students
 print_header
-print(students)
+# print(students)
+sort_by_cohort(students)
 print_footer(students)
