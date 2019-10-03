27bc7ab1ea61dec6e30e1572befd76587dcee039
Marc Blenkinsop <marcblenky@hotmail.com>
2019-10-03 15:49:44 +0100

    Tidied up the code

--- directory.rb
+++ directory.rb
@@ -2,33 +2,28 @@ def input_students
     puts "Please enter the name of the student followed by the cohort and student age: "
     puts "To finish, just hit return twice"
     students = []
-    name = gets.chomp.capitalize
-    cohort = gets.chomp.to_sym.upcase
-    age = gets.chomp
+    name = gets.strip.capitalize
+    cohort = gets.strip.to_sym.upcase
+    age = gets.strip
 
     while !name.empty? do
-
             if cohort.empty?
                 cohort = "unknown"
             end
-
             if age.empty?
                 age = "unknown"
             end
-
         students << {name: name, cohort: cohort, age: age}
-
                 if students.count < 2
                     puts "We only have 1 student"
                 else 
                     puts "Now we have #{students.count} students"
                 end
-
         puts "More students?"
-        name = gets.chomp.capitalize
+        name = gets.strip.capitalize
             break if name.empty?
-        cohort = gets.chomp.to_sym.upcase
-        age = gets.chomp
+        cohort = gets.strip.to_sym.upcase
+        age = gets.strip
     end
     students
 end
@@ -39,7 +34,6 @@ def print_header
 end
 
 def print(students)
-
     i = 0 
     while i < students.size
         puts "#{i+1}. #{students[i][:name]} (#{students[i][:cohort]} cohort) (#{students[i][:age]})".center(50)
@@ -48,17 +42,21 @@ def print(students)
 end
 
 def print_footer(students)
-    puts "Overall we have #{students.count} great students.".center(50)
+    if students.count > 1
+        puts "Overall we have #{students.count} great students.".center(50)
+    else
+        puts "We have one great student".center(50)
+    end
 end
 
 def sort_by_cohort(students)
-    # puts "Which Cohort would you like to see? "
-    # cohort = gets.chomp.to_sym
-    students.sort_by { |student| student[:cohort] }.map { |student| puts "#{student[:name]} - #{student[:cohort]} - #{student[:age]}".center(50) }
+    puts " "
+    puts "Students Sorted By Cohort".center(50)
+    students.sort_by { |student| student[:cohort] }.map { |student| puts"#{student[:name]} - #{student[:cohort]} - #{student[:age]}".center(50) }
 end
 
 students = input_students
 print_header
-# print(students)
+print(students)
 sort_by_cohort(students)
 print_footer(students)
